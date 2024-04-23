ad6644639462d1820d665abe11cf49e51088071e
LeonardoFeMartins <leonardomartins2697@gmail.com>
2024-04-22 21:48:09 -0300

    Criando Cardapio

---
+++ Hamburgueria.DATA/ViewModel/CardapioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Hamburgueria.DATA.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburgueria.DATA.ViewModel
+{
+    public class CardapioViewModel
+    {
+        public IList<PratosDTO> PratosL { get; set; } = new List<PratosDTO>();
+        public PratosDTO Prato { get; set; }
+    }
+}
---
+++ Hamburgueron & Onicham/Controllers/CardapioController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Hamburgueria.DATA.DTO;
+using Hamburgueria.DATA.Interfaces.IServices;
+using Hamburgueria.DATA.Models;
+using Hamburgueria.DATA.Service;
+using Hamburgueria.DATA.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HamburgueriaTriagem.Controllers
+{
+    public class CardapioController : Controller
+    {
+        private readonly IPratosService _pratosService;
+        private readonly IMapper _mapper;
+
+        public CardapioController(IPratosService pratosService, IMapper mapper)
+        {
+            _pratosService = pratosService;
+            _mapper = mapper;
+        }
+
+
+        public IActionResult Index()
+        {
+            var buscarPratos = _pratosService.ListarPratos();
+
+            var listarPratosDto = _mapper.Map<List<PratosDTO>>(buscarPratos);
+
+            CardapioViewModel vm = new()
+            {
+                PratosL = listarPratosDto,
+                Prato = new PratosDTO()
+            };
+
+            return View(vm);
+            
+        }
+
+        public IActionResult AdicionarPedidoCliente(int pratoId)
+        {
+            var prato = _pratosService.GetPratoById(pratoId);
+
+            CardapioViewModel vm = new()
+            {
+                Prato = new PratosDTO
+                {
+                    Codigo = pratoId,
+                    NomePrato = prato.NomePrato,
+                    ValorPrato = Convert.ToString(prato.ValorPrato),
+                }
+            };
+
+            return View(vm);
+
+        }
+
+
+
+
+
+
+
+    }
+}
